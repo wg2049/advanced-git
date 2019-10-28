5c9d055996c3fb7c6a891dd6cb3fcf741b6bbc15
wg2049 <wg2049@hotmail.com>
2019-10-28 11:50:36 +0800

    Revert "revert 9dfb38fc7f9a33299801c695092de3e2e8b67607"
    This reverts commit 2ce57d5d0eb77496837393b437990112bccfa965.

--- Product.java
+++ Product.java
@@ -3,6 +3,5 @@ public class Product {
         System.out.println("I'm version 10.0!");
 #        System.out.println("I have a bug!");
         System.out.println("A bad feature!");
-        System.out.println("I'm version 8.0!");
     }
 } 
