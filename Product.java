e24e035838cf19a71f7ef5522860ee4886381690
wg2049 <wg2049@hotmail.com>
2019-10-28 11:43:41 +0800

    fixed a bug

--- Product.java
+++ Product.java
@@ -1,7 +1,7 @@
 public class Product {
     public static void main(String[] args) {
         System.out.println("I'm version 10.0!");
-        System.out.println("I have a bug!");
+#        System.out.println("I have a bug!");
         System.out.println("A bad feature!");
     }
 } 
